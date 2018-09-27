3167e1f5272f6b3e4a62c2af73737346af809688
evgeniStanchev <evgeni.danailov.stanchev@gmail.com>
2018-09-27 13:19:52 +0300

    Add files via upload

---
+++ DynamicProxy/src/main/java/com/egtinteractive/proxy/DynamicProxy.java
@@ -0,0 +1,30 @@
+package com.egtinteractive.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+public final class DynamicProxy implements InvocationHandler {
+
+    private final Map<?, ?> map;
+
+    public DynamicProxy(final Map<?, ?> map) {
+	this.map = map;
+    }
+
+    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+	method.setAccessible(true);
+	final String methodName = method.getName();
+	final Date date = new Date();
+	final String startsAt = new SimpleDateFormat("HH:mm:ss:SSS").format(date);
+	final double startTime = System.nanoTime();
+	final Object result = method.invoke(map, args);
+	final double endTime = System.nanoTime();
+	final double duration = (endTime - startTime) / 1000000;
+	System.out.printf("Method:%s" + System.lineSeparator() + "Start:%s" + System.lineSeparator() + "Duration:%f"
+		+ System.lineSeparator() + System.lineSeparator(), methodName, startsAt, duration);
+	return result;
+    }
+}
---
+++ DynamicProxy/src/main/java/com/egtinteractive/proxy/Test.java
@@ -0,0 +1,39 @@
+package com.egtinteractive.proxy;
+
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class Test {
+    public static void main(String[] args) {
+	final Class<Test> testClass = Test.class;
+	final ClassLoader classLoader = testClass.getClassLoader();
+	final Class<?>[] interfaces = new Class[] { Map.class };
+	final Map<String, Integer> map = new HashMap<>();
+	final DynamicProxy dynamicProxy = new DynamicProxy(map);
+
+	@SuppressWarnings("unchecked")
+	final Map<String, Integer> proxy = (HashMap<String, Integer>) Proxy.newProxyInstance(classLoader, interfaces,
+		dynamicProxy);
+
+	final String str1 = UUID.randomUUID().toString();
+	final String str2 = UUID.randomUUID().toString();
+	final String str3 = UUID.randomUUID().toString();
+	final int num1 = ThreadLocalRandom.current().nextInt();
+	final int num2 = ThreadLocalRandom.current().nextInt();
+	final int num3 = ThreadLocalRandom.current().nextInt();
+
+	proxy.put(str1, num1);
+	proxy.put(str2, num2);
+	proxy.put(str3, num3);
+
+	proxy.containsKey(str1);
+	proxy.containsKey(UUID.randomUUID().toString());
+	proxy.remove(str1);
+	proxy.keySet();
+	proxy.get(str1);
+	proxy.get(UUID.randomUUID().toString());
+    }
+}
